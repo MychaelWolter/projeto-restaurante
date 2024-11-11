7d60415601a43da4fbd261409fa646cdfa30a7ef
Mychael Lucio Wolter dos Santos <mychaelwolter1025@gmail.com>
2024-11-11 16:25:28 -0400

    uptade

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+function menuButton(){
+    const menu = document.getElementById("menu-list");
+    if(menu.style.display === "none" && window.innerWidth <= 500){
+       menu.style.display = "block";
+    }else{
+        menu.style.display = "none";
+    };
+};
+
 function mensagemMail(){
     const mensagem = document.getElementById("span-mensagem");
     mensagem.style.display = "block";
